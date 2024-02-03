309b95d03ea6f89da157601167793c2bf1c8b948
Lungu Daniel <daniel.lungu@flip.ro>
2024-02-03 10:51:46 +0200

    modificari

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function showMessage(response) {
   if (response === "No") {
     const noButton = document.getElementById("no-button");
-    const container = document.querySelector(".container");
     const maxWidth = window.innerWidth - noButton.offsetWidth;
     const maxHeight = window.innerHeight - noButton.offsetHeight;
 
@@ -12,19 +11,27 @@ function showMessage(response) {
     document.getElementsByClassName("image")[0].src = "images/gun.gif";
 
     // Generate random coordinates within the visible container
-    const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
-    const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
+    let randomX = 0;
+    let randomY = 0;
+    const generateRandomCoordinates = () => {
+      randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
+      randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
+      noButton.style.left = randomX + "px";
+      noButton.style.top = randomY + "px";
+    }
 
     // Apply new coordinates to the button
-    noButton.style.left = randomX + "px";
-    noButton.style.top = randomY + "px";
+    generateRandomCoordinates();
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "The no button is just for visuals";
+        "The no button is just for visuals";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
+    setTimeout(() => {
+      generateRandomCoordinates();
+    }, 500);
   }
 
   if (response === "Yes") {
